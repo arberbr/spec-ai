e77f3c244ae3ba2113acbbff8c2bb6e2f0830ec4
Adrian Hajdin - JS Mastery <contact@jsmastery.pro>
2026-04-28 12:50:53 +0200

    implement code-rabbit suggested fixes

--- components/editor/project-dialogs.tsx
+++ components/editor/project-dialogs.tsx
@@ -10,7 +10,7 @@ import {
 } from "@/components/ui/dialog"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import type { useProjectActions } from "@/hooks/use-project-actions"
+import { useProjectActions } from "@/hooks/use-project-actions"
 
 type ProjectDialogsProps = ReturnType<typeof useProjectActions>
 
